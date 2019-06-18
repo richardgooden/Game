4baa6a3b65d31b866dbe891b1ab909e1668cac3c
richardgooden <richard.gooden5@gmail.com>
2019-06-18 06:35:02 -0500

    menu

---
+++ Space Crew/Assets/mainmenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void Playgame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+}
