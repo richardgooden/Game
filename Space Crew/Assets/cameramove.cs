d7b3bb373eb6c4bdf1549f2c27ee123b8d156a84
richardgooden <richard.gooden5@gmail.com>
2019-06-18 06:51:44 -0500

    edited camera script

--- Space Crew/Assets/cameramove.cs
+++ Space Crew/Assets/cameramove.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class cameramove : MonoBehaviour
 {
-    public float speed; 
+    public float speed;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +15,16 @@ public class cameramove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Mouse1))
-        {
+       // if(Input.GetKey(KeyCode.Mouse1))
+      //  {
+            this.transform.position = player.position;
             transform.Rotate(0f, Input.GetAxis("Mouse X") * speed * Time.deltaTime, 0f);
-            
-        }
+
+      //  }
+      //  else
+      //  {
+     //       this.transform.position = player.position;
+      //  }
         
     }
 }
